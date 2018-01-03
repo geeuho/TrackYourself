453b63ec1ac58899fb9eb5031959e87fb6317485
elaine01 <ecumming.g@gmail.com>
2018-01-02 22:34:48 -0800

    sync local with master

--- assets/javascript/oneday.js
+++ assets/javascript/oneday.js
@@ -1,33 +1,110 @@
 // Globals
 var apiKey = 'a6ad2fabb4d3a05e6dbd5453734d01d8';
-var appId = '4ca70c03'
+var appId = '4ca70c03';
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCtZlEIl3iT_WHQdOvvxzu8LyFjueJUg5A",
+	authDomain: "trackyourself-1e739.firebaseapp.com",
+	databaseURL: "https://trackyourself-1e739.firebaseio.com",
+	projectId: "trackyourself-1e739",
+	storageBucket: "",
+	messagingSenderId: "345964565415"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
 
 // Listeners
 $('#food-submit-btn').on('click', function(){
+	// Clear save messages
+	$('#save-messages').empty();
+
 	// Get meal time
 	var mealTime = $('#meal-time-input option:selected').text();
 	var quantity = $('#quantity-input').val().trim();
 	var food = $('#food-input').val().trim();
 
 	populateFoodTable(mealTime, quantity, food);
+
+	// Clear inputs
+	$('#quantity-input').val('');
+	$('#food-input').val('');
 });
 
 $('#exercise-submit-btn').on('click', function(){
-		var durationMins = $('#exercise-duration-input').val().trim();
-		var exercise = $('#exercise-dropdown-input option:selected').text();
+	// Clear save messages
+	$('#save-messages').empty();
 
-		if(exercise.length === 0) {
-			exercise = $('#exercise-string-input').val().trin();
-		}
+	var durationMins = $('#exercise-duration-input').val().trim();
+	var exercise = $('#exercise-dropdown-input option:selected').text();
 
-		// Appending the word 'minutes' to duration input in order
-		// to facilitate Nutritionix natural language API
-		durationMins = durationMins + ' minutes';
+	if(exercise.length === 0) {
+		exercise = $('#exercise-string-input').val().trim();
+	}
 
-		populateExerciseTable(durationMins, exercise);
+	// Appending the word 'minutes' to duration input in order
+	// to facilitate Nutritionix natural language API
+	durationMins = durationMins + ' minutes';
+
+	populateExerciseTable(durationMins, exercise);
+
+	// Clear inputs
+	$('#exercise-duration-input').val('');
+
+	if($('#exercise-string-input').val().length > 0) {
+		$('#exercise-string-input').val('');
+	} else {
+		$($('#exercise-dropdown-input').children()[0]).prop('selected', true);
+	}
 });
 
 $(document).on('click', '.delete-row', removeRowFromTable);
+
+$('#save').on('click', function(event) {
+	event.preventDefault();
+
+	// Check if there is anything to save
+	var foodResults = $('#food-results').children('tr');
+
+	var exerciseResults = $('#exercise-results').children('tr');
+
+	var span = $('<span></span>');
+	
+	if(foodResults.length === 0) {
+		
+		var br = $('<br>');
+
+		span.text('No food data to save.');
+		
+		$('#save-messages').append(span);
+		
+		$('#save-messages').append(br);
+
+	} else {
+		saveFoodRows(foodResults);
+
+		span.text('Food data saved.');
+		
+		$('#save-messages').append(span);
+	}
+
+	span = $('<span></span>');
+
+	if(exerciseResults.length === 0) {
+		span.text('No exercise data to save.');
+		
+		$('#save-messages').append(span);
+
+	} else {
+		saveExerciseRows(exerciseResults);
+
+		span.text('Exercise data saved.');
+		
+		$('#save-messages').append(span);
+	}
+});
 
 function populateFoodTable(mealTime, quantity, food) {
 	var userInput = mealTime + ' ' + quantity + ' ' + food;
@@ -54,21 +131,25 @@ function populateFoodTable(mealTime, quantity, food) {
 		    // New cell
 		    var td = $('<td></td>');
 		    td.text(mealTime)
+		    td.attr('value', 'mealTime');
 		    tr.append(td);
 
 		    // New cell Food
 		    var td = $('<td></td>');
 		    td.text(food.food_name);
+		    td.attr('value', 'foodName');
 		    tr.append(td);
 
 		    // New cell Quantity
 		    td = $('<td></td>');
 		    td.text(food.serving_qty);
+		    td.attr('value', 'servingQty');
 		    tr.append(td);
 
 		    // New cell Calories
 		    td = $('<td></td>');
 		    td.text(food.nf_calories);
+		    td.attr('value', 'calories');
 		    tr.append(td);
 
 		    // New cell, contains delete button
@@ -113,20 +194,25 @@ function populateExerciseTable(durationInMinutes, exercise) {
 			// New cell Time
 			var td = $('<td></td>');
 			td.text(exercise.duration_min);
+			td.attr('value', 'durationMins');
 			tr.append(td);
 
 			// New cell Type
 			td = $('<td></td>');
 			td.text(exercise.name);
+			td.attr('value', 'exerciseType');
 			tr.append(td);
 
 			// Empty cell Distance
 			td = $('<td></td>');
+			td.text('');
+			td.attr('value', 'distance');
 			tr.append(td);
 
 			// New cell Calories Burned
 			td = $('<td></td>');
 			td.text(exercise.nf_calories);
+			td.attr('value', 'caloriesBurned');
 			tr.append(td);
 
 			// New cell, contains delete button
@@ -150,4 +236,38 @@ function removeRowFromTable() {
 	var rowToRemove = $(this).parent().parent();
 
 	$(rowToRemove).remove();
+}
+
+function saveFoodRows(foodResults) {
+	var todaysDate = moment($('#date').text(), 'ddd MMM DD YYYY');
+
+	var dataObj = {};
+
+	for(var i=0; i<foodResults.children().length; i++) {
+		if($(foodResults.children()[i]).text() === 'Delete') {
+			database.ref('/' + todaysDate.format('x') + '/food').push().set(dataObj);
+
+			// Create a new object for the next set of data
+			dataObj = {};
+		} else {
+			dataObj[$(foodResults.children()[i]).attr('value')] = $(foodResults.children()[i]).text();
+		}		
+	}
+}
+
+function saveExerciseRows(exerciseResults) {
+	var todaysDate = moment($('#date').text(), 'ddd MMM DD YYYY');
+
+	var dataObj = {};
+
+	for(var i=0; i<exerciseResults.children().length; i++) {
+		if($(exerciseResults.children()[i]).text() === 'Delete') {
+			database.ref('/' + todaysDate.format('x') + '/exercise').push().set(dataObj);
+
+			// Create a new object for the next set of data
+			dataObj = {};
+		} else {
+			dataObj[$(exerciseResults.children()[i]).attr('value')] = $(exerciseResults.children()[i]).text();
+		}		
+	}
 }
